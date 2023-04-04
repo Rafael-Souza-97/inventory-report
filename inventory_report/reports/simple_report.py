dedb5112f4a5f6552966f40f077ccdf2c0d2b880
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-04 18:53:05 -0300

    feat: req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from collections import Counter
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data):
+        fabr_mais_ant = min(product["data_de_fabricacao"] for product in data)
+        menor_validade = min(
+            (product["data_de_validade"] for product in data if product
+                ["data_de_validade"] >= datetime.now().strftime("%Y-%m-%d"))
+        )
+
+        empresas = [product["nome_da_empresa"] for product in data]
+        mais_produtos = Counter(empresas).most_common(1)[0][0]
+
+        report = (
+            f"Data de fabricação mais antiga: {fabr_mais_ant}\n"
+            f"Data de validade mais próxima: {menor_validade}\n"
+            f"Empresa com mais produtos: {mais_produtos}"
+        )
+
+        return report
