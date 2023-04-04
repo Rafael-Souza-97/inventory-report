6d5d685a53d0764d8dd07e13055a36498ff4a13b
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-04 19:55:58 -0300

    feat: req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,16 @@
+from .simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(data):
+        simple_report = SimpleReport.generate(data)
+        empresas = [produto["nome_da_empresa"] for produto in data]
+        produtos_por_empresa = Counter(empresas)
+        report = f"{simple_report}\nProdutos estocados por empresa:\n"
+        for empresa in set(empresas):
+            quantidade = produtos_por_empresa[empresa]
+            report += f"- {empresa}: {quantidade}\n"
+
+        return report
